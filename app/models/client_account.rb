b50cf904fdf46a14e4999c5a729baa738fba59cd
Titilope Apuabi <tyaps002@gmail.com>
2023-02-09 12:50:46 +0100

    Establishing relationaships

--- app/models/client_account.rb
+++ app/models/client_account.rb
@@ -1,4 +1,5 @@
 class ClientAccount < ApplicationRecord
     has_many :doctor through: :client_review
     has_many :client_review
+    has_one :appointment
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,7 @@
 class Doctor < ApplicationRecord
      has_many :specialization through: :doctor_specialization
      has_many :client_account through: :client_review
-     has_many :office
+     has_many :hospital_affiliation
+     has_many :office through: :hospital_affiliation
+     has_many :qualification
 end
