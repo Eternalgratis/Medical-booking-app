89bf4b0efd46052cc3a2681f46c80a6e9e8c3b7b
Titilope Apuabi <tyaps002@gmail.com>
2023-02-14 23:37:45 +0100

    Created a database

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
-  has_many: client_review
+  has_many :client_review
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
